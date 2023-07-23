54a8e9a6fdc69a33c377e889279d933d15ccaebe
pujangga123 <rush_x@yahoo.com>
2023-07-23 23:36:07 +0700

    hapus yang tidak perlu

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
     define('PATH_LIBS', "libs/");
 
     define("DB_HOST", "localhost");
-    define("DB_USER", "userredlist");
-    define("DB_PASSWORD", "restaurant");
-    define("DB_NAME", "redlistdb");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "");
+    define("DB_NAME", "redlist");
